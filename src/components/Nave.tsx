b566c3f9e7ac1c4543f60a33283095d0fbaa92fb
arrifat346afs <arrifat1235@gmail.com>
2026-03-29 23:02:15 +0600

    Updates: 2026-03-29T17:02:13.439Z

--- src/components/Nave.tsx
+++ src/components/Nave.tsx
@@ -1,10 +1,15 @@
-
+import { Moon, Sun } from "lucide-react"
+import { Button } from "./ui/button"
 
 export default function Nave() {
   return (
-  <nav>
-    <h1>Neumorphism UI</h1>
-  </nav>
+    <nav>
+      <h1>Neumorphism UI</h1>
+      <Button variant="outline" size="icon">
+        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    </nav>
   )
 }
-
