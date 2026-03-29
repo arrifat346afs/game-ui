b1cda4fc014be3f89c339f89894e3da766123930
arrifat346afs <arrifat1235@gmail.com>
2026-03-29 12:46:06 +0600

    Updates: 2026-03-29T06:46:04.552Z

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export function App() {
   return (
-    <div className="bg-zinc-200 flex flex-col w-screen h-screen gap-5 p-5">
+    <div className=" flex flex-col w-screen h-screen gap-5 p-5">
       {/* Row 1 */}
       <Button variant="default" >why</Button>
       <Button variant="destructive">why</Button>
