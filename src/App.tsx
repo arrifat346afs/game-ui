ced272702417a248a6dcd37baa2288eb6f3405c9
arrifat346afs <arrifat1235@gmail.com>
2026-03-29 12:33:13 +0600

    Updates: 2026-03-29T06:33:11.228Z

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 export function App() {
   return (
-    <div className="flex min-h-svh  items-center justify-center gap-16 p-12 ">
+    <div className="flex min-h-svh  items-center justify-center gap-16 p-12 bg-zinc-100">
       {/* Row 1 */}
       <Button variant="default" >why</Button>
       {/* <button className="">why</button> */}
