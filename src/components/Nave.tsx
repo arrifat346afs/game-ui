e6928c9d106f8fee0f2f7f83cc9d2a37e93b6d25
arrifat346afs <arrifat1235@gmail.com>
2026-03-29 23:23:39 +0600

    Updates: 2026-03-29T17:23:37.358Z

--- src/components/Nave.tsx
+++ src/components/Nave.tsx
@@ -17,9 +17,6 @@ export default function Nave() {
         ) : (
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
         )}
-        {/* <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" /> */}
-        <span className="sr-only">Toggle theme</span>
       </Button>
     </nav>
   )
