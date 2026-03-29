7cfaeeb97e8364f4c3df3ad4aaec00ff4940aca2
arrifat346afs <arrifat1235@gmail.com>
2026-03-29 12:35:27 +0600

    Updates: 2026-03-29T06:35:25.781Z

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-background  ",
+        default: "bg-background  text-primary ",
         outline:
           "border-border hover:bg-input/50 hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:bg-input/30",
         secondary:
