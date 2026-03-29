6fa1553075381959ecb15069843da46ddcad7160
arrifat346afs <arrifat1235@gmail.com>
2026-03-29 23:15:12 +0600

    Updates: 2026-03-29T17:15:10.283Z

--- src/components/Nave.tsx
+++ src/components/Nave.tsx
@@ -7,9 +7,9 @@ export default function Nave() {
   return (
     <nav className="flex items-center justify-between py-4 px-12" >
       <h1 className="font-bold">Neumorphism UI</h1>
-      <Button onClick={() => setTheme("dark")} size="icon">
-        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Button  size="icon">
+        <Sun onClick={() => setTheme("dark")} className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        <Moon onClick={() => setTheme("light")} className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
         <span className="sr-only">Toggle theme</span>
       </Button>
     </nav>
