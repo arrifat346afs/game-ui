3d0ac89535507330720be69a7837d385f74b9ece
arrifat346afs <arrifat1235@gmail.com>
2026-03-29 23:03:31 +0600

    Updates: 2026-03-29T17:03:29.892Z

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@/components/ui/button"
+import Nave from "./components/Nave"
 
 export function App() {
   return (
-    <div className="p-4 flex gap-2">
-      {/* Row 1 */}
-      <Button variant="default" >Submit</Button>
-      <Button variant="destructive">Delete</Button>
-      <Button disabled>Disabled</Button>
-    </div>
+    <main>
+      <Nave />
+      <div className="flex gap-2 p-4">
+        {/* Row 1 */}
+        <Button variant="default">Submit</Button>
+        <Button variant="destructive">Delete</Button>
+        <Button disabled>Disabled</Button>
+      </div>
+    </main>
   )
 }
 
