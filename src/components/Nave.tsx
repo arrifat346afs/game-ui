b2980561cae5057ddc06719e70dbd804f191570f
arrifat346afs <arrifat1235@gmail.com>
2026-03-29 23:02:55 +0600

    Updates: 2026-03-29T17:02:53.540Z

--- src/components/Nave.tsx
+++ src/components/Nave.tsx
@@ -5,7 +5,7 @@ export default function Nave() {
   return (
     <nav>
       <h1>Neumorphism UI</h1>
-      <Button variant="outline" size="icon">
+      <Button  size="icon">
         <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
         <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
         <span className="sr-only">Toggle theme</span>
