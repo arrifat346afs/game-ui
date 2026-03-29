ef0de06d2fb63c73a5058bdb68919949f2367ac1
arrifat346afs <arrifat1235@gmail.com>
2026-03-29 12:00:03 +0600

    style(button-styling): switch to native button with inline classes and update neumorphic shadow styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ export function App() {
     <div className="flex min-h-svh  items-center justify-center gap-16 p-12 bg-[#e0e0e0]">
       {/* Row 1 */}
       <Button variant="default">why</Button>
+      <button className="">why</button>
      
     </div>
   )
